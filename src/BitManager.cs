0ad20d577b57d686a1ad9e477f70c4ea1059a0a3
bscal <jonfus123@hotmail.com>
2024-04-04 00:48:47 -0400

    updated version

--- src/BitManager.cs
+++ src/BitManager.cs
@@ -66,7 +66,7 @@ public struct User
 public partial class BitManager : Node2D
 {
 	public const int VersionMajor = 0;
-	public const int VersionMinor = 9;
+	public const int VersionMinor = 10;
 	public static string VERSION_STRING = string.Format("{0}.{1}", VersionMajor, VersionMinor);
 
 	public const int MAX_BITS = 1024;
